16427d164c377630b415cdffe57b9fa4d14be111
prebenohre <prebenohre@gmail.com>
2025-06-27 15:04:54 +0200

    added sprite shape

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import pygame
 from constants import *
 
 def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -12,10 +20,9 @@ def main():
             
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) /1000
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
 if __name__ == "__main__":
     main()
